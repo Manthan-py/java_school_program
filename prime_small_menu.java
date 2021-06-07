2e2ad7353b3341de1785e3d87e9183c0bc64749c
Manthan-py <84665669+Manthan-py@users.noreply.github.com>
2021-06-07 14:18:24 +0530

    Added prime_small_menu program
    This program is a menu-driven program and helps user to :-
    1) To find whether a number is prime or composite.
    2) To get the smallest digit from a number.

---
+++ prime_small_menu.java
@@ -0,0 +1,48 @@
+import java.util.Scanner ;
+public class prime_small_menu
+{
+    public static void main()
+    {
+        System.out.println("*********************************Menu*********************************");
+        System.out.println("1. To check the number is prime or composite.");
+        System.out.println("2. To get the smallest digit from the number.");
+        System.out.println("*********************************Menu*********************************");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch(choice) 
+        {
+            case 1:
+            System.out.println("Enter the number to check : ");
+            int num_check = sc.nextInt();
+            int p=0;
+            for (int i=1;i<=num_check;i++)
+            {
+                if (num_check%i==0) 
+                {
+                    p++;
+                }
+            }
+            if (p<=2) {
+                System.out.println("The entered number is prime");
+                System.out.println("*****************************Thank you*****************************");
+            }
+            else {
+                System.out.println("The entered number is composite");
+                System.out.println("*****************************Thank you*****************************");
+            }
+            break;
+            case 2:
+            System.out.println("Enter the number to give its smallest digit");
+            int num = sc.nextInt();
+            int smallest = 9;
+            while (num!=0) {
+                int small = num%10;
+                smallest = Math.min(small,smallest);
+                num/=10;
+            }
+            System.out.println("The smallest digit is " + smallest);
+            System.out.println("*****************************Thank you*****************************");
+            break;
+        }
+    }
+}
