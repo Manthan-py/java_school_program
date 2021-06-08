5bacc57135dd37ebb9254a53ef4c1ad5dee15599
Manthan-py <84665669+Manthan-py@users.noreply.github.com>
2021-06-08 08:38:17 +0530

    Modified the code and added default function.

--- prime_small_menu.java
+++ prime_small_menu.java
@@ -12,37 +12,40 @@ public class prime_small_menu
         switch(choice) 
         {
             case 1:
-            System.out.println("Enter the number to check : ");
-            int num_check = sc.nextInt();
-            int p=0;
-            for (int i=1;i<=num_check;i++)
-            {
-                if (num_check%i==0) 
+                System.out.println("Enter the number to check : ");
+                int num_check = sc.nextInt();
+                int p=0;
+                for (int i=1;i<=num_check;i++)
                 {
-                    p++;
+                    if (num_check%i==0) 
+                    {
+                        p++;
+                    }
                 }
-            }
-            if (p<=2) {
-                System.out.println("The entered number is prime");
-                System.out.println("*****************************Thank you*****************************");
-            }
-            else {
-                System.out.println("The entered number is composite");
-                System.out.println("*****************************Thank you*****************************");
-            }
-            break;
+                if (p<=2) {
+                    System.out.println("The entered number is prime");
+                    System.out.println("*****************************Thank you*****************************");
+                }
+                else {
+                    System.out.println("The entered number is composite");
+                    System.out.println("*****************************Thank you*****************************");
+                }
+                break;
             case 2:
-            System.out.println("Enter the number to give its smallest digit");
-            int num = sc.nextInt();
-            int smallest = 9;
-            while (num!=0) {
-                int small = num%10;
-                smallest = Math.min(small,smallest);
-                num/=10;
-            }
-            System.out.println("The smallest digit is " + smallest);
-            System.out.println("*****************************Thank you*****************************");
-            break;
+                System.out.println("Enter the number to give its smallest digit");
+                int num = sc.nextInt();
+                int smallest = 9;
+                while (num!=0) {
+                    int small = num%10;
+                    smallest = Math.min(small,smallest);
+                    num/=10;
+                }
+                System.out.println("The smallest digit is " + smallest);
+                System.out.println("*****************************Thank you*****************************");
+                break;
+            default :
+                System.out.println("Enter a valid menu number")
+                System.out.println("*****************************Thank you*****************************")
         }
     }
 }
